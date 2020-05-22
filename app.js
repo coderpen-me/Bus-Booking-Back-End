3975ea74dcedc59fb9d8659e279a7bf715fb1df5
coderpen-me <coderpen@gmail.com>
2020-05-22 17:40:49 +0530

    -- updated security authentication

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ app.use('/api', routes);
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
-
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
